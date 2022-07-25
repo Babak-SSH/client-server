8d654ad77fe1758c151094a0135fb5b222e7a883
Babak-SSH <babak.sefidgar@gmail.com>
2022-07-25 19:49:56 +0430

    Add: fail and sucess ret_t

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,10 +3,10 @@
 #include "../include/utils.h"
 
 
-ret_st ret_st::failure(const std::string &msg) {
+TCP::ret_st TCP::ret_st::failure(const std::string &msg) {
     return ret_st(false, msg);
 }
 
-ret_st ret_st::success(const std::string &msg) {
+TCP::ret_st TCP::ret_st::success(const std::string &msg) {
     return ret_st(true, msg);
 }
