d81c5a3045f39f62f99ab8db3c81d2d03284e182
Babak-SSH <babak.sefidgar@gmail.com>
2022-07-25 19:42:37 +0430

    Add: event handlers and message and disconnect event

--- include/client.h
+++ include/client.h
@@ -6,19 +6,29 @@
 #include <atomic>
 #include <pthread.h>
 #include <thread>
+#include <functional>
 
 
 #include "file_descriptor.h"
 
 
 namespace TCP {
+
+    enum ClientEvent {
+        DISCONNECTED,
+        INCOMING_MSG
+    };
+
     class Client {
+        
+        using client_event_handler_t = std::function<void(const Client&, ClientEvent, const std::string&)>;
+
         private:
             FileDesc::FileDescriptor _sockfd;
             std::string _ip = "";
             std::atomic<bool> _isConnected;
             std::thread * _receiveThread = nullptr;
-            //client_event_handler_t _eventHandlerCallback;
+            client_event_handler_t _eventHandlerCallback;
 
             void setConnected(bool flag) { _isConnected = flag; }
 
@@ -34,8 +44,8 @@ namespace TCP {
             void setIp(const std::string & ip) { _ip = ip; }
             std::string getIp() const { return _ip; }
 
-            // void setEventsHandler(const client_event_handler_t & eventHandler) { _eventHandlerCallback = eventHandler; }
-            // void publishEvent(ClientEvent clientEvent, const std::string &msg = "");
+            void setEventsHandler(const client_event_handler_t & eventHandler) { _eventHandlerCallback = eventHandler; }
+            void publishEvent(ClientEvent clientEvent, const std::string &msg = "");
 
             bool isConnected() const { return _isConnected; }
 
